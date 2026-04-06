db6ebdb98a776b912e5cc6a6d48dc88dd7dfaef6
Mohammed Alorf <mohammed1alorf@gmail.com>
2026-04-06 09:54:07 +0300

    Complete TODO 6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
 
 // TODO 1: Define server port
 const app = express();
@@ -17,9 +18,16 @@ app.use(morgan("dev"));
 
 
 // TODO 6.1: Create root route "/"
+app.get("/", (req, res) => {
+  res.send("Welcome to the Random Quote API");
+});
 
 
 // TODO 6.2: Create "/api/quote" route
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 
 // TODO 7: Start server using app.listen
