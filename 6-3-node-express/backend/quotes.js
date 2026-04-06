c5e53fc6506942a0ab9948b0cc4bb6ecf3b15b6f
Mohammed Alorf <mohammed1alorf@gmail.com>
2026-04-06 09:52:52 +0300

    Complete TODO 3

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,4 +1,5 @@
 // Import random.js file
+import { getRandomInt } from "./utils/random.js";
 
 const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
@@ -9,4 +10,7 @@ const quotes = [
 
 // TODO 3: Create and export getRandomQuote function
 //Hint: export function getRandomQuote(){}
-
+export function getRandomQuote() {
+  const index = getRandomInt(quotes.length);
+  return quotes[index];
+}
