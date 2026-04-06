1619f737d30ec4ea3eda994df6d43112cf88b0f5
Mohammed Alorf <mohammed1alorf@gmail.com>
2026-04-06 09:53:42 +0300

    Complete TODO 5

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,6 +1,7 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 // TODO 1: Define server port
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors());
 
 
 // TODO 5: Import and apply Morgan middleware
+app.use(morgan("dev"));
 
 
 // TODO 6.1: Create root route "/"
